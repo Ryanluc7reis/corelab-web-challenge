c3d61645d2eabe68b74811cd186f5eb263c10f71
ryanluc7reis <ryan.dev18@outlook.com>
2024-07-10 03:27:00 -0300

    Criado FileUpload

---
+++ src/components/fileupload/FileUpload.jsx
@@ -0,0 +1,144 @@
+import React, { useContext, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import styled from 'styled-components'
+import axios from 'axios'
+import { useSWRConfig } from 'swr'
+
+import ConfirmDelete from '../cofirmdelete/ConfirmDelete'
+import { PopUpContext } from '../../context/useContextPopUp'
+
+const getColor = (props) => {
+  if (props.isDragAccept) {
+    return '#00e676'
+  }
+  if (props.isDragActive) {
+    return '#2196f3'
+  }
+  return '#eeeeee'
+}
+const FileContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+const DropzoneContainer = styled.div`
+  border: 2px dotted ${(props) => getColor(props)};
+  padding: 20px;
+  text-align: center;
+  cursor: pointer;
+`
+const BoxFile = styled.div`
+  border: 2px double #3af321;
+  border-radius: 15px;
+  padding: 20px;
+  text-align: center;
+  cursor: pointer;
+  :hover {
+    scale: 0.9;
+  }
+`
+const TextContainer = styled.div`
+  display: flex;
+  padding: 6px;
+  align-items: center;
+  font-family: sans-serif;
+  font-size: 13px;
+  color: #474747da;
+  justify-content: space-between;
+`
+const Image = styled.img`
+  padding: 1px;
+  cursor: pointer;
+`
+
+export const FileUpload = ({ id, file, children, onClick }) => {
+  const [showPopUp, setShowPopUp, messageType, setMessageType] = useContext(PopUpContext)
+  const [confirmDelete, setConfirmDelete] = useState(false)
+
+  const URI_API = process.env.API_URI
+  const { mutate } = useSWRConfig()
+
+  const handleConfirmDelete = () => {
+    setConfirmDelete(!confirmDelete)
+  }
+
+  const { getRootProps, getInputProps, isDragActive, isDragAccept } = useDropzone({
+    onDrop: async (acceptedFiles) => {
+      if (acceptedFiles.length === 0) return
+
+      const formData = new FormData()
+      acceptedFiles.forEach((file) => {
+        formData.append('file', file)
+        formData.append('id', id)
+      })
+
+      try {
+        const response = await axios.patch(`${URI_API}/files/editFile`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        if (response.status === 200) {
+          mutate(`${URI_API}/getNotes`)
+          mutate(`${URI_API}/getFavoritesNotes`)
+          setShowPopUp(true)
+          setMessageType('createdFile')
+        }
+      } catch (error) {
+        console.error('Upload failed', error)
+        setMessageType('error')
+      }
+    }
+  })
+  const handleDelete = async () => {
+    try {
+      const response = await axios.patch(`${URI_API}/files/editFileToDelete`, {
+        id,
+        src: ''
+      })
+
+      if (response.status === 200) {
+        mutate(`${URI_API}/getNotes`)
+        mutate(`${URI_API}/getFavoritesNotes`)
+        setConfirmDelete(false)
+        setShowPopUp(true)
+        setMessageType('deletedFile')
+      }
+    } catch (err) {
+      console.error(err.message)
+      setMessageType('error')
+    }
+  }
+
+  const seeYourFile = () => {
+    const fileName = file.split('\\').pop()
+    if (file) {
+      window.open(`${URI_API}/seeFile/${fileName}`)
+    }
+  }
+  return (
+    <FileContainer>
+      {file ? (
+        <>
+          <TextContainer>
+            <p>Clique para ver seu arquivo</p>
+            <Image onClick={handleConfirmDelete} src="x.png" />
+          </TextContainer>
+          <BoxFile onClick={seeYourFile}>{children}</BoxFile>
+        </>
+      ) : (
+        <DropzoneContainer {...getRootProps({ isDragActive, isDragAccept })}>
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p>Solte os arquivos aqui..</p>
+          ) : (
+            <p>Clique ou arraste arquivos aqui para fazer upload</p>
+          )}
+        </DropzoneContainer>
+      )}
+      {confirmDelete && (
+        <ConfirmDelete isFile onClickDelete={handleDelete} onClickClose={handleConfirmDelete} />
+      )}
+    </FileContainer>
+  )
+}
